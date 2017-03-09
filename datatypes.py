e7fe8596cdc5f3305606e83e509a4ff3b9822eb2
Faith <faijazkivuva@gmail.com>
2017-03-08 18:30:28 -0800

    corrected datatypes problem

--- datatypes.py
+++ datatypes.py
@@ -1,26 +1,27 @@
 
-def data_type(faith): 
+def data_type(checkdata_type): 
 	
-    if faith is None:
+    if checkdata_type is None:
         return 'no value'
        
-    if isinstance(faith, str):
-        return len(faith)
+    if isinstance(checkdata_type, str):
+        return len(checkdata_type)
       
-    elif isinstance(faith, bool):
-        return faith
+    elif isinstance(checkdata_type, bool):
+        return checkdata_type
         
-    elif isinstance(faith, int):
+    elif isinstance(checkdata_type, int):
     	
-        if faith < 100:
+        if checkdata_type < 100:
             return "less than 100"
-        elif faith == 100:
+        elif checkdata_type == 100:
             return "equal to 100"
         return "more than 100"
         
-    elif type(faith) == list:
-		    if len(faith) < 3:
+    elif type(checkdata_type) == list:
+		    if len(checkdata_type) < 3:
 			      return None
 		    else:
-			      return faith[2]
+			      return checkdata_type[2]
+print(data_type(45))
 			  
