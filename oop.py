6e66d9e4c6567603ef44fdd8f639dae31bdfd167
Faith <faijazkivuva@gmail.com>
2017-03-09 07:21:24 -0800

    OOP real problem corrected

--- oop.py
+++ oop.py
@@ -1,27 +1,38 @@
-class Box():
-	"""box is the main object"""
-	volume=0
-	vol_matchbox=0
-	length=0
-	width=0
-	height=0
-	def __init__(self, length=0,width=0,height=0):
-		self.length=length
-		self.width=width
-		self.height=height
+class Box(object):
+ 
+    length=int(input("Please enter the Length: "))
 
-	volume=length*width*height
-	print(volume)
+    width=int(input("Please enter the Widtht:"))
 
+    height=int(input("Please enter the Height: "))
 
-	class Matchbox(Box):
-		"""match box inhrits some fatures form box like length,width  and height"""
-		def __init__(self, l,w,h):
-			super(Matchbox, self).__init__()
-			self.lenth = l
-			self.width=w
-			self.height=h
-	vol_matchbox=(l*w*h)
-	print(vol_matchbox)
+    def __init__(self, length=0,width=0,height=0):
 
-	
+        self.length=length
+
+        self.width=width
+
+        self.height=height
+
+    volume=length*width*height
+
+    print("The volume is:")
+
+    print(volume)
+    
+class matchbox(Box):
+    "class matchbox inheriting from class Box"
+
+    def __init__(self, l,w,h):
+
+     super().__init__(length,width,height)
+
+     self.length=l
+
+     self.width=w
+
+     self.height=h
+   
+     volume_Of_matchbox=l*w*h
+     
+     print(volume_0f_room)
